13e0dbe2ec91ab711e6130035fa03ad915a05e6e
Willington Vega <wvega@wvega.com>
2021-02-07 18:09:29 -0500

    Add setup script for SSH configuration

---
+++ ssh/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# vim: set syntax=bash
+
+directory=$(dirname $(realpath $0))
+
+mkdir -p "${HOME}/.ssh/config.d"
+
+# symlink public configuration files
+for file in "${directory}"/config.d/*.config; do
+    symlink "${file}" "${HOME}/.ssh/config.d/$(basename $file)"
+done
+
+# symlink private configuration files
+for file in "$(dirname $directory)"/private/ssh/config.d/*.config; do
+    symlink "${file}" "${HOME}/.ssh/config.d/$(basename $file)"
+done
+
+symlink "${directory}/config" "${HOME}/.ssh/config"
