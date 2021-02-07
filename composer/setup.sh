b206c75f91e894140b1b481b3c3b8ab0c1891b6e
Willington Vega <wvega@wvega.com>
2021-02-07 17:02:17 -0500

    Symlink global composer.json and composer.lock

--- composer/setup.sh
+++ composer/setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # vim: set syntax=bash
 
+directory=$(dirname $(realpath $0))
+
 if ! { which composer >/dev/null 2>&1; }; then
     echo "Installing Composer 1"
     curl -sS https://getcomposer.org/installer | php -- --1 --install-dir=/usr/local/bin --filename=composer
 fi
+
+symlink "${directory}/composer.json" "${HOME}/.composer/composer.json"
+symlink "${directory}/composer.lock" "${HOME}/.composer/composer.lock"
