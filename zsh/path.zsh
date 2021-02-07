da992a6eb96e9f701f78bcb311a1b20b6ec6f66e
Willington Vega <wvega@wvega.com>
2021-02-07 17:16:16 -0500

    Add script with ZSH path and language environment variables

---
+++ zsh/path.zsh
@@ -0,0 +1,16 @@
+# Homebrew
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+
+# Go development
+export GOPATH="${HOME}/.go"
+export GOROOT="/usr/local/opt/go/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+
+# Add path to binaries from mysql@5.7 formula
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
+# Custom binaries
+export PATH="$HOME/bin:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$HOME/.rvm/bin:$PATH"
