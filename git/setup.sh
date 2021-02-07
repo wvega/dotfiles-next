c17e843626700d9007f8b2c77deb6e66dc12374d
Willington Vega <wvega@wvega.com>
2021-02-07 18:47:44 -0500

    Add setup script for Git configuration

---
+++ git/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# vim: set syntax=bash
+
+directory=$(dirname $(realpath $0))
+
+mkdir -p "${HOME}/.git.d"
+
+# # symlink public configuration files
+# for file in "$(dirname $directory)"/git/git.d/*.gitconfig; do
+#     symlink "${file}" "${HOME}/.git.d/$(basename $file)"
+# done
+
+# symlink private configuration files
+for file in "$(dirname $directory)"/private/git/git.d/*.gitconfig; do
+    symlink "${file}" "${HOME}/.git.d/$(basename $file)"
+done
+
+symlink "${directory}/gitconfig" "${HOME}/.gitconfig"
