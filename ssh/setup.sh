e1b70865496abee8b8566b4f6b6b21b39bfd1ab0
Willington Vega <wvega@wvega.com>
2021-02-07 18:25:12 -0500

    Do not try to symlink public SSH configuration files
    We don't have any yet!

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -5,10 +5,10 @@ directory=$(dirname $(realpath $0))
 
 mkdir -p "${HOME}/.ssh/config.d"
 
-# symlink public configuration files
-for file in "${directory}"/config.d/*.config; do
-    symlink "${file}" "${HOME}/.ssh/config.d/$(basename $file)"
-done
+# # symlink public configuration files
+# for file in "${directory}"/config.d/*.config; do
+#     symlink "${file}" "${HOME}/.ssh/config.d/$(basename $file)"
+# done
 
 # symlink private configuration files
 for file in "$(dirname $directory)"/private/ssh/config.d/*.config; do
