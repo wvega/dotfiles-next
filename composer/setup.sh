0b55c2dbfd876dde7788ef0a1ff2b08c6a4b929f
Willington Vega <wvega@wvega.com>
2021-02-07 17:03:59 -0500

    Switch to PHP 7.4 if the default is PHP 8.0 or newer
    Some of our dependencies still require or work better on PHP 7.4.

--- composer/setup.sh
+++ composer/setup.sh
@@ -10,3 +10,9 @@ fi
 
 symlink "${directory}/composer.json" "${HOME}/.composer/composer.json"
 symlink "${directory}/composer.lock" "${HOME}/.composer/composer.lock"
+
+if [[ "0" == $(php -r "echo version_compare(PHP_VERSION, '8', '>=') ? 0 : 1;") ]]; then
+    echo "Setting PHP 7.4 as the default version"
+    brew unlink php
+    brew link php@7.4
+fi
