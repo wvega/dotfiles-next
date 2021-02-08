83f8a6fc52d616605df8a056caa1c3021e2a1606
Willington Vega <wvega@wvega.com>
2021-02-07 21:17:00 -0500

    Add setup script to install binaries

---
+++ bin/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# vim: set syntax=bash
+
+mkdir -p "${HOME}/bin"
+
+# install WP-CLI
+if [[ ! -f /usr/local/bin/wp ]]; then
+    echo "Installing WP-CLI"
+    curl --output /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x /usr/local/bin/wp
+fi
+
+# install wpv
+if [[ ! -f "${HOME}/bin/wpv" ]]; then
+    echo "Installing wpv"
+    curl --output "${HOME}/bin/wpv" https://raw.githubusercontent.com/smilingrobots/wpv/master/wpv.sh
+    chmod u+x "${HOME}/bin/wpv"
+fi
