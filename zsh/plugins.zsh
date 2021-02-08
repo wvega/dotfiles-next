3693c00d5251a18925e8650297b972b3e45430e2
Willington Vega <wvega@wvega.com>
2021-02-07 21:16:09 -0500

    Update ZSH configuration

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,4 +1,12 @@
 #!/usr/bin/env zsh
 
+# use the Up and Down arrow keys to traverse history based on what has already been entered at the prompt
+# https://unix.stackexchange.com/a/97844
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
+
 # load autojump
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
