e7beb81eb930c8d753168e39acb150eb4bd07844
Willington Vega <wvega@wvega.com>
2021-03-07 17:16:58 -0500

    Include Git aliases

--- git/setup.sh
+++ git/setup.sh
@@ -5,10 +5,10 @@ directory=$(dirname $(realpath $0))
 
 mkdir -p "${HOME}/.git.d"
 
-# # symlink public configuration files
-# for file in "$(dirname $directory)"/git/git.d/*.gitconfig; do
-#     symlink "${file}" "${HOME}/.git.d/$(basename $file)"
-# done
+# symlink public configuration files
+for file in "$(dirname $directory)"/git/git.d/*.gitconfig; do
+    symlink "${file}" "${HOME}/.git.d/$(basename $file)"
+done
 
 # symlink private configuration files
 for file in "$(dirname $directory)"/private/git/git.d/*.gitconfig; do
