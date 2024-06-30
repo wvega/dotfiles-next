078c93913f404224f646ef407ca7fa7574717c97
Willington Vega <wvega@wvega.com>
2024-06-30 12:36:57 -0500

    Install WP CLI Rename DB Prefix package during setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,6 +10,8 @@ if [[ ! -f /usr/local/bin/wp ]]; then
     echo "Installing WP-CLI"
     curl --output /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x /usr/local/bin/wp
+
+    wp package install iandunn/wp-cli-rename-db-prefix
 fi
 
 # install wpv
